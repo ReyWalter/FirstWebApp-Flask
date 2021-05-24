75a2f315f7e53a52cd842873093521ce015e4d11
ReyWalter <84330918+ReyWalter@users.noreply.github.com>
2021-05-24 12:04:58 -0400

    Deployed web app on Gcloud Server

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 @app.route("/") # Home page
 
 def index():
-    return "Congratulations, it's a web app!" # Returns a string to the web page
+    return "<h1>Hello World. I wanna be come a good Python developer</h1>" # Returns a string to the web page
+
+@app.route("/<celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="8080", debug=True) # Start server
